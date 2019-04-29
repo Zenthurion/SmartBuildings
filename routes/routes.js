f7ed8ef60e6d77df4f1c9d5aff8f950033a2aac8
Markus <markus.mjak@gmail.com>
2019-04-29 15:03:19 +0200

    Minor fix

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,10 @@
 var appRouter = (app) => {
     app.get('/temp', (req, res) => {
-        let value = Math.floor(Math.random() * 6) + 1
-        res.sendStatus(value.toString())
-        console.log(value)
+        let value = Math.floor(Math.random() * 60) - 20
+        res.send(value.toString())
     })
     app.get('/', (req, res) => {
-        res.sendStatus("Hello world!")
-        console.log("Hello World")
+        res.send("Hello world!")
     })
 }
 
