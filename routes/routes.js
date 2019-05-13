f419db387461212220fe5b4fb3effe8aeeac0ac3
Markus <markus.mjak@gmail.com>
2019-05-13 09:04:30 +0200

    simple xml output

--- routes/routes.js
+++ routes/routes.js
@@ -93,17 +93,119 @@ var appRouter = (app) => {
         if(schedule.hasOwnProperty(room)) {
             if(schedule[room].hasOwnProperty(day)){
                 if(time >= 0 && time < 24) {
-                    res.send('' + schedule[room][day][time])
+                    res.send('' + json2xml(schedule[room][day][time]))
                     return
                 }
-                res.send('' + schedule[room][day])
+                res.send('' + json2xml(schedule[room][day]))
                 return
             }
-            res.send('' + schedule[room])
+            res.send('' + json2xml(schedule[room]))
             return
         }
         res.send(schedule)
     })
+    app.get('/schedule', (req, res) => {
+        res.send('' + json2xml(schedule))
+    })
 }
+
+function Simulator() {
+    
+    this.timeStep
+    this.state = {
+        dim: {
+            x: 5,
+            y: 5,
+            z: 3
+        }, 
+        volume: 75,
+        occupancy: 0,
+        temperature: 18,
+        co2: 100,
+        ventilation: {
+            fans: {
+                count: 3,
+                watts: 50,
+                ability: 550,
+                effect: 0
+            }
+        },
+        heating: {
+            effect: 0
+        }
+    }
+
+    this.simulationStates = []
+
+    this.simulate = (initialState, from, to, timeStep) => {
+        state = initialState
+        this.timeStep = timeStep
+
+    }
+
+    function update(time, previousState){
+        var stepState = JSON.parse(JSON.stringify(previousState))
+        time += timeStep
+        stepState.occupancy = occupancy(time, stepState)
+        stepState.co2 = co2(time, stepState)
+
+
+    }
+
+    function occupancy(time, stepState) {
+
+    }
+
+    function co2(time, stepState) {
+        
+    }
+}
+
+/*	This work is licensed under Creative Commons GNU LGPL License.
+
+	License: http://creativecommons.org/licenses/LGPL/2.1/
+   Version: 0.9
+	Author:  Stefan Goessner/2006
+	Web:     http://goessner.net/ 
+*/
+function json2xml(o, tab) {
+    var toXml = function(v, name, ind) {
+       var xml = "";
+       if (v instanceof Array) {
+          for (var i=0, n=v.length; i<n; i++)
+             xml += ind + toXml(v[i], name, ind+"\t") + "\n";
+       }
+       else if (typeof(v) == "object") {
+          var hasChild = false;
+          xml += ind + "<" + name;
+          for (var m in v) {
+             if (m.charAt(0) == "@")
+                xml += " " + m.substr(1) + "=\"" + v[m].toString() + "\"";
+             else
+                hasChild = true;
+          }
+          xml += hasChild ? ">" : "/>";
+          if (hasChild) {
+             for (var m in v) {
+                if (m == "#text")
+                   xml += v[m];
+                else if (m == "#cdata")
+                   xml += "<![CDATA[" + v[m] + "]]>";
+                else if (m.charAt(0) != "@")
+                   xml += toXml(v[m], m, ind+"\t");
+             }
+             xml += (xml.charAt(xml.length-1)=="\n"?ind:"") + "</" + name + ">";
+          }
+       }
+       else {
+          xml += ind + "<" + name + ">" + v.toString() +  "</" + name + ">";
+       }
+       return xml;
+    }, xml="";
+    for (var m in o)
+       xml += toXml(o[m], m, "");
+    return tab ? xml.replace(/\t/g, tab) : xml.replace(/\t|\n/g, "");
+ }
+ 
 
 module.exports = appRouter
